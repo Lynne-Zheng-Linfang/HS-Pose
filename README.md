28e4438e11d45f3289d26f8133299cab6270efc8
Lynne <zhenglinfang@icloud.com>
2023-04-10 13:07:13 +0800

    Improved README.md

--- README.md
+++ README.md
@@ -94,7 +94,7 @@ python -m evaluation.evaluate --dataset_dir YOUR_DATA_DIR --detection_dir DETECT
 ```
 
 ## Example Code
-You can run the following training and testing commands to get the results below.
+You can run the following training and testing commands to get the results similar to the below table.
 ```shell
 python -m engine.train --model_save output/models/HS-Pose/ --num_workers 20 --batch_size 16 --train_steps 1500 --seed 1677330429
 python -m evaluation.evaluate  --model_save output/models/HS-Pose/model_149 --resume 1 --resume_model ./output/models/HS-Pose/model_149.pth --eval_seed 1677483078
--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0
-python -m engine.train --model_save output/models/HS-Pose/ --num_workers 20 --batch_size 16 --train_steps 1500 --seed 1677330429
-python -m evaluation.evaluate  --model_save output/models/HS-Pose/model_149 --resume 1 --resume_model ./output/models/HS-Pose/model_149.pth --eval_seed 1677483078
+export CUDA_VISIBLE_DEVICES=1
+python -m engine.train --model_save output/models/HS-Pose_workers32/ --num_workers 32 --batch_size 16 --train_steps 1500 --seed 1677330429
+python -m evaluation.evaluate  --model_save output/models/HS-Pose_workers32/model_149 --resume 1 --resume_model ./output/models/HS-Pose_workers32/model_149.pth --eval_seed 1677483078 --num_workers 32
